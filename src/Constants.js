7f5b1966d753e9ef30704d83fd61a0285171cf0c
Daniel Torrecillas <22879920+danieltorrecillas@users.noreply.github.com>
2022-01-24 21:33:16 -0600

    Add constants

---
+++ src/Constants.js
@@ -0,0 +1,10 @@
+const constants = {
+  unitTestTikTokTrackingUrl: 'https://vm.tiktok.com/TTPdh8FAMo',
+  unitTestFirstRedirectedTikTokTrackingUrl: 'https://m.tiktok.com/v/7031216065820609797.html?_d=secCgYIASAHKAESPgo8L%2Bk%2FOYtlCAdvAtBx7Fsf73WqTKuJE0nzLwqZASN7O4PorscSsK%2BP9BT3jXLmyMjzOYl5VCvFvdykKpHWGgA%3D&checksum=05e3eb41b2f8157e3e5f724e8f62490e79830b7835bf9844fe463348971e1524&language=en&preview_pb=0&sec_user_id=MS4wLjABAAAA7bZmBn9kvyVQ3sO82vkuf6zP5M4k66jD5dow_R3tpPTj8hqFdJlpwZLc1WOuZMej&share_app_id=1233&share_item_id=7031216065820609797&share_link_id=D7F36D80-0CA7-448E-8392-E268D65DB5C2&source=h5_m&timestamp=1642998941&tt_from=copy&u_code=dmiigj1fafh62b&user_id=7052521007568258053&utm_campaign=client_share&utm_medium=ios&utm_source=copy',
+  unitTestAnonymizedUrl: 'https://m.tiktok.com/v/7031216065820609797',
+  integrationTestTikTokTrackingUrl: 'https://vm.tiktok.com/TTPdMcXEWw',
+  integrationTestFirstRedirectedTikTokTrackingUrl: 'https://m.tiktok.com/v/7022710108492696837.html?_d=secCgYIASAHKAESPgo8V0F9faJOh%2Fo2WaFDDauPPWvQaMNTyYfXKZ1YqYSAcBcwSQjkFa%2F%2Bn78iTArx20q4y8aVtqoa%2B48rUXnmGgA%3D&checksum=a58e2ee2a8493e4d3fd798d0346f95d973dbcf35cdb1bde087934d3d1e07f158&language=en&preview_pb=0&sec_user_id=MS4wLjABAAAA7bZmBn9kvyVQ3sO82vkuf6zP5M4k66jD5dow_R3tpPTj8hqFdJlpwZLc1WOuZMej&share_app_id=1233&share_item_id=7022710108492696837&share_link_id=16318BA5-6E80-480C-9A67-026991D0B22A&source=h5_m&timestamp=1642043802&tt_from=copy&u_code=dmiigj1fafh62b&user_id=7052521007568258053&utm_campaign=client_share&utm_medium=ios&utm_source=copy',
+  integrationTestAnonymizedUrl: 'https://m.tiktok.com/v/7022710108492696837',
+}
+
+module.exports = constants
